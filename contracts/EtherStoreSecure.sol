d38563e77cd9f1c80b62a76062b12e3ac426b0b0
QuickTriXXX <isthisreal@whatever.com>
2022-06-15 18:06:07 +0100

    initial commit

---
+++ contracts/EtherStoreSecure.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract EtherStoreSecure {
+	// initialize the mutex
+	bool reEntrancyMutex = false;
+	uint256 public withdrawalLimit = 1 ether;
+	mapping(address => uint256) public lastWithdrawTime;
+	mapping(address => uint256) public balances;
+
+	function depositFunds() public payable {
+		balances[msg.sender] += msg.value;
+	}
+
+	function withdrawFunds(uint256 _weiToWithdraw) public {
+		require(!reEntrancyMutex);
+		require(balances[msg.sender] >= _weiToWithdraw);
+		// limit the withdrawal
+		require(_weiToWithdraw <= withdrawalLimit);
+		// limit the time allowed to withdraw
+		require(block.timestamp >= lastWithdrawTime[msg.sender] + 1 weeks);
+		balances[msg.sender] -= _weiToWithdraw;
+		lastWithdrawTime[msg.sender] = block.timestamp;
+		// set the reEntrancy mutex before the external call
+		reEntrancyMutex = true;
+		payable(msg.sender).transfer(_weiToWithdraw);
+		// release the mutex after the external call
+		reEntrancyMutex = false;
+	}
+}
